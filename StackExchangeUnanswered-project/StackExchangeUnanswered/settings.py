f1cff140d5d4552394047fa3bf860860c9240c41
Ravin D <ravin.d3107@outlook.com>
2022-10-29 13:27:16 +0530

    Home page setup is successful!

--- StackExchangeUnanswered-project/StackExchangeUnanswered/settings.py
+++ StackExchangeUnanswered-project/StackExchangeUnanswered/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "finder.apps.FinderConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'StackExchangeUnanswered.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
