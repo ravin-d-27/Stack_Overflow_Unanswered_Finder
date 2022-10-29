0f4cd7a190d717a70f8853f76b0e4be8ebe6eac8
Ravin D <ravin.d3107@outlook.com>
2022-10-29 13:54:29 +0530

    Extraction of API is done

--- StackExchangeUnanswered-project/finder/views.py
+++ StackExchangeUnanswered-project/finder/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import json
+import requests
 
-# Create your views here.
 
 def home(request):
+    response = requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
     return render(request, "finder/home.html")
