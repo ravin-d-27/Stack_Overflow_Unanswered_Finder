16448ecbb9a4d933729226bd8203a304e4e568fc
Ravin D <ravin.d3107@outlook.com>
2022-11-12 21:44:30 +0530

    Back End is ready! Lets go with the front end now

--- StackExchangeUnanswered-project/finder/views.py
+++ StackExchangeUnanswered-project/finder/views.py
@@ -5,4 +5,25 @@ import requests
 
 def home(request):
     response = requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
-    return render(request, "finder/home.html")
+    lst = []
+    for data in response.json()['items']:
+        if (data["answer_count"]==0):
+            rec = []
+            rec.append(data['title'])
+            rec.append(data['link'])
+            string = ""
+
+            for i in data["tags"]:
+                string+="#"+i+" "
+            rec.append(string)
+
+            usertype = data["owner"]["display_name"]
+            profile = data["owner"]["profile_image"]
+
+            rec.append(usertype)
+            rec.append(profile)
+
+            lst.append(rec)
+        else:
+            pass
+    return render(request, "finder/home.html", {"resp":lst})
