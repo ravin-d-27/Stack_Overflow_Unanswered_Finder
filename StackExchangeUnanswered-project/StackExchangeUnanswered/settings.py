a5ef3f28eee04626b3ef78001c2be1801c1a51ee
Ravin D <ravin.d3107@outlook.com>
2022-11-13 22:08:55 +0530

    The Static folder is ready. Add the files into the static folder which is inside StackExchangeUnanswered folder

--- StackExchangeUnanswered-project/StackExchangeUnanswered/settings.py
+++ StackExchangeUnanswered-project/StackExchangeUnanswered/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR,'StackExchangeUnanswered/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- StackExchangeUnanswered-project/finder/views.py
+++ StackExchangeUnanswered-project/finder/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import json
 import requests
 
 
