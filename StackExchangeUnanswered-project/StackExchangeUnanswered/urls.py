84651c651b7d89726764592fa98951eb5b5c9f3a
Ravin D <ravin.d3107@outlook.com>
2022-11-14 09:06:07 +0530

    static url has been setup in urlpatterns

--- StackExchangeUnanswered-project/StackExchangeUnanswered/urls.py
+++ StackExchangeUnanswered-project/StackExchangeUnanswered/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from finder import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
